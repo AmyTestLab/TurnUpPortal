c2c2be3e78b378bc5e8ea2d594bc5d9503c8d82b
Amy Sara Baby <amysarababy7@gmail.com>
2024-11-21 15:33:01 +1300

    Login and Create Time and Record automated test built

---
+++ TurnUpPortal/Program.cs
@@ -0,0 +1,102 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //open chrome browser
+        IWebDriver driver = new ChromeDriver();
+
+
+        //launch turnupportal
+        driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+        driver.Manage().Window.Maximize();
+
+
+        //identify username textbox and enter valid username 
+        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+        usernameTextbox.SendKeys("hari");
+
+
+        //identify password textbox and enter valid password 
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+
+
+        //identify login button and click it
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginButton.Click();
+
+        //check if user has logged in successfully
+        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+        if (helloHari.Text == "Hello hari!")
+        {
+            Console.WriteLine("USER HAS LOGGED IN SUCCESSFULLY. TEST PASSED");
+        }
+        else
+        {
+            Console.WriteLine("USER HAS NOT LOGGED IN SUCCESSFULLY.TEST FAILED");
+        }
+        //create a time and material record
+
+
+        Thread.Sleep(3000);
+        //navigate to time and material module
+        IWebElement administrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+        administrationTab.Click();
+
+        IWebElement timeAndMaterialOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+        timeAndMaterialOption.Click();
+
+        //click on create new button
+        IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+        createNewButton.Click();
+
+        //select time from drop down
+        IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+        typeCodeDropdown.Click();
+
+        Thread.Sleep(2000);
+        IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+        timeOption.Click();
+
+        //type code into code textbox
+        IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+        codeTextbox.SendKeys("TA Programme");
+
+        //type description into description textbox
+
+        IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+        descriptionTextbox.SendKeys("This is a description");
+
+        //type price  into price textbox
+        IWebElement priceTagOverlap = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+        priceTagOverlap.Click();
+
+
+        IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+        priceTextbox.SendKeys("12");
+
+        //click on save button
+        IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+        saveButton.Click();
+        Thread.Sleep(3000);
+
+        //check if  time record has been created successfully
+        IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+        goToLastPageButton.Click();
+
+        IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+        if (newCode.Text == "TA Programme")
+        {
+            Console.WriteLine("TIME RECORD CREATED.");
+        }
+        else
+        {
+            Console.WriteLine("NEW TIME RECORD HAS NOT BEEN CREATED.");
+        }
+    }
+}
